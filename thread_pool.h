1540aa0a41a971c82093c394593b7b134ac2c127
Maksim Ruts <tribar.ge@gmail.com>
2022-03-17 00:26:13 +0300

    Adds thread joiner, replaces fire_once with func_wrapper

--- thread_pool.h
+++ thread_pool.h
@@ -5,47 +5,66 @@
 #ifndef THREAD_POOL_THREAD_POOL_H
 #define THREAD_POOL_THREAD_POOL_H
 
-#include "fire_once.h"
+#include "func_wrapper.h"
+#include "join_threads.h"
 #include "thread_queue.h"
 #include <atomic>
 #include <future>
 #include <thread>
 #include <vector>
 
-using task_t = fire_once<int()>;
-
 class thread_pool
 {
 public:
+    thread_pool();
+    ~thread_pool();
     template <typename FuncType>
-    std::future<typename std::result_of<FuncType()>::type> submit(FuncType F);
+    std::future<typename std::result_of<FuncType()>::type> submit(FuncType f);
 
 private:
     std::atomic_bool done;
-    thread_queue<task_t> tqueue;
+    thread_queue<function_wrapper> tqueue;
     std::vector<std::thread> threads;
+    join_threads joiner;
 
     void worker_thread();
 };
 
-void thread_pool::worker_thread()
+thread_pool::thread_pool()
+    : done(false)
+    , joiner(threads)
 {
-    while(!done) {
-        task_t task;
-        if(tqueue.try_pop(task)) {
-            int res = std::move(task)();
-        }
+    auto thread_count = std::thread::hardware_concurrency();
+    for(auto i = 0; i < thread_count; ++i) {
+        threads.push_back(std::thread(&thread_pool::worker_thread, this));
     }
 }
 
+thread_pool::~thread_pool()
+{
+    done = true;
+}
+
 template <typename FuncType>
-std::future<type> thread_pool::submit(FuncType F)
+std::future<typename std::result_of<FuncType()>::type> thread_pool::submit(FuncType f)
 {
     typedef typename std::result_of<FuncType()>::type result_type;
     std::packaged_task<result_type()> task(std::move(f));
     std::future<result_type> res(task.get_future());
     tqueue.push(std::move(task));
     return res;
+}
+
+void thread_pool::worker_thread()
+{
+    while(!done) {
+        function_wrapper task;
+        if(tqueue.try_pop(task)) {
+            task();
+        } else {
+            std::this_thread::yield();
+        }
+    }
 }
 
 #endif //THREAD_POOL_THREAD_POOL_H
